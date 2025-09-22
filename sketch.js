32813366dd3c96d17b60a371c5b76edb2f4332a6
Shukutoku Advance <97524834+TechByAdvance@users.noreply.github.com>
2025-09-22 18:23:30 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,25 @@
+let x;
+let y;
+let ballCount = 20; // ボールの個数をここで指定
+
 function setup() {
     createCanvas(400, 400);
+    x = 0;
+    y = [];
+    for (let i = 0; i < ballCount; i++) {
+        // 上端0、下端heightにぴったり並ぶように等間隔で配置
+        y.push(i * (height / (ballCount - 1)));
+    }
 }
 
 function draw() {
-    background(220);
+    background(0, 0, 0, 20);
+    x = x + 10;
+    if (x > width) {
+        x = 0
+    }
+    fill(128, 255, 255);
+    for (let i = 0; i < y.length; i++) {
+        circle(x, y[i], random(10))
+    }
 }
